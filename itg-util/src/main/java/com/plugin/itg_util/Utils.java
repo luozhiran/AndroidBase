838a64ba1e4d072e9c34e2ee40e73d3a09e249bb
luozhiran <luozhiran@em-data.com.cn>
2021-01-04 13:42:35 +0800

    反选

--- itg-util/src/main/java/com/plugin/itg_util/Utils.java
+++ itg-util/src/main/java/com/plugin/itg_util/Utils.java
@@ -1,6 +1,15 @@
 package com.plugin.itg_util;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Looper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.core.app.ActivityCompat;
 
 public class Utils {
 
@@ -15,6 +24,30 @@ public class Utils {
     public static void ensureChangeOnMainThread() {
         if (Thread.currentThread() != Looper.getMainLooper().getThread()) {
             throw new IllegalStateException("You must only modify the ObservableList on the main thread.");
+        }
+    }
+
+
+
+    public static String[] checkPermission(Context context, String[] permissions) {
+        List<String> list = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            list = new ArrayList<>();
+            for (String s : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, s) != PackageManager.PERMISSION_GRANTED) {
+                    list.add(s);
+                }
+            }
+        }
+        if (list != null) {
+            return list.toArray(new String[]{});
+        }
+        return null;
+    }
+
+    public static void requestPermission(String[] needPermission, Activity activity, int code) {
+        if (needPermission != null && needPermission.length > 0) {
+            ActivityCompat.requestPermissions(activity, needPermission, 11);
         }
     }
 }
